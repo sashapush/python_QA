b61a53429b1417a907130815a97d5bea2601a879
Alexander <sashapush@tut.by>
2023-02-08 14:22:38 +0100

    Parsing of json file(in file system)

--- api/BackEndAutomation/jsonParse.py
+++ api/BackEndAutomation/jsonParse.py
@@ -3,10 +3,20 @@ import json
 # json library is used for deserialising json
 json_file = '{"name": "Axel","languages":["Java","Python"]}'
 
-# loads parses json string to dictionary;
-ass = json.loads(json_file)
-print(type(ass))  # dictionary
-print(ass)
-print(ass["name"])
-print(ass["languages"])  # returns list
-print(ass["languages"][0])  # returns values
+# loads parses json **string** to dictionary;
+# ass = json.loads(json_file)
+# print(type(ass))  # dictionary
+# print(ass)
+# print(ass["name"])
+# print(ass["languages"])  # returns list
+# print(ass["languages"][0])  # returns values
+
+
+# parse json file**
+with open(
+        "C:\\Users\\Alex\\PycharmProjects\\pythonAQAdraft\\api\\BackEndAutomation\\ass.json") as f:  # without param it opens in read mode
+    file = json.load(f)  # we create a new dictionary from file
+    print(type(file))  # dictionary
+    # print(file) #file content
+    print(file["result"]["chart"][1]["score"])
+    print(file["result"])
