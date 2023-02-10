d8fc4294bc45e1a839c344c0d98c92894f5b1d24
Alexander <sashapush@tut.by>
2023-02-10 17:16:40 +0100

    tested authentication

--- api/BackEndAutomation/postRequest.py
+++ api/BackEndAutomation/postRequest.py
@@ -2,6 +2,7 @@ import requests
 from api.BackEndAutomation.payloads import *  # import * imports all methods instead of specific one
 from api.BackEndAutomation.utils.configs import getConfig
 from api.BackEndAutomation.utils.resources import apiResources
+import os
 
 url = getConfig()['API']['endpoint'] + apiResources.addBook
 headers = {"Content-Type": "application/json"}
@@ -30,3 +31,16 @@ d = delete_response.json()
 assert delete_response.status_code == 200
 print("Assert success - status code 200")
 assert d["msg"] == "book is successfully deleted"
+
+# Authentication to github
+# print(os.environ)
+ass = os.environ.get('GPASS')
+gurl = 'https://api.github.com/user'
+git_response = requests.get(gurl, auth=("sashapush@tut.by", ass))  # attribute verify=False will skip ssl sertificate
+print("Git got status code: ", git_response.status_code)
+
+# TODO more testing with httpbin.org
+url = 'https://httpbin.org/post'
+files = {'file': open('Library-API.docx', 'rb')}
+r = requests.post(url, files=files)
+print(r.text)
