4feba6f0af415b8354bf65b94fbef2ef3c2195d0
Alexander <sashapush@tut.by>
2023-02-11 14:43:28 +0100

    Timeouts and redirects(disabled) in requests object

--- api/BackEndAutomation/Misc.py
+++ api/BackEndAutomation/Misc.py
@@ -3,7 +3,15 @@ import requests
 # to send cookies we need to use dictionary:
 cookie = {"visit-month": "June"}
 
-r = requests.get("https://rahulshettyacademy.com/", cookies=cookie)
+r = requests.get("http://rahulshettyacademy.com/", allow_redirects=False, cookies=cookie, timeout=50)
+# allow_redirects=False will not allow redirects, stops after first status code
+# timeout is used in case endpoint has high load and repsonds after a set amount of time
+
+# REDIRECTS history
+# as it turned out there is a redirect before hiting this url #302-200
+print(r.history)  # will return 301
+print(type(r.history))
+# assert [<Response [301]>] in r.history
 print(r.status_code)
 
 s = requests.session()
