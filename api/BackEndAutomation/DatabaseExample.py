85df663f41f69e18b7afd1b2ee75ec3bc33304d2
Alexander <sashapush@tut.by>
2023-02-13 17:20:16 +0100

    refactored mysql connection: moved connection data to properties.ini, created a logic  to get those properties in configs.py created a method to connect to database using new method and data from properties.ini
    now db connection is done via getDbConnection()

--- api/BackEndAutomation/DatabaseExample.py
+++ api/BackEndAutomation/DatabaseExample.py
@@ -1,7 +1,10 @@
 import mysql.connector
 
+from api.BackEndAutomation.utils.configs import *
+
 # host, database/schema name, user, password #### PythonAutomation/ apidevelop
-conn = mysql.connector.connect(host="localhost", database="apidevelop", user="root", password="root")
+# conn = mysql.connector.connect(host="localhost", database="apidevelop", user="root", password="root")
+conn = getDbConnection()  # optimised version
 print(conn.is_connected())  # checking if connection is successfully established
 
 cursor = conn.cursor()  # creating a stream between python and database
@@ -15,14 +18,16 @@ sum = 0
 for item in table:
     sum += item[2]
 print(sum)
+assert sum == 340
 query = "update customerInfo set Location = %s where CourseName = %s;"
 data = ("US", "Jmeter")
-# or f"update customerInfo set Location = {location} where CourseName = {name};"
+#or f"update customerInfo set Location = {location} where CourseName = {name};"
 cursor.execute(query, data)
 conn.commit()  # commits current transaction, used with update
 print("Update successful")
 cursor.execute(f"delete from customerInfo where courseName = 'WebServices';")
 conn.commit()
 print("Delete successful")
+
 
 conn.close()  # close connection to database server
--- api/BackEndAutomation/utils/configs.py
+++ api/BackEndAutomation/utils/configs.py
@@ -1,6 +1,9 @@
 import configparser
 import os
 
+import mysql.connector
+from mysql.connector import Error
+
 
 def getConfig():
     config = configparser.ConfigParser()
@@ -8,6 +11,24 @@ def getConfig():
     return config
 
 
+db_connect_config = {
+    "host": getConfig()['SQL']['host'],
+    "database": getConfig()['SQL']['database'],
+    "user": getConfig()['SQL']['user'],
+    "password": getConfig()['SQL']['password']
+}
+
+
 def getPassword():
     password = os.environ.get('GPASS')
     return password
+
+
+def getDbConnection():
+    try:
+        conn = mysql.connector.connect(**db_connect_config)
+        if conn.is_connected():
+            print("Connection established successfully")
+            return conn
+    except Error as e:
+        print(e)
