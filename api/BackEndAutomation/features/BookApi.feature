029add4520879cd88e6f090886e135fb9eb757f0
Alexander <sashapush@tut.by>
2023-02-15 16:21:15 +0100

    added filter to after_scenario hook - now it's executed only for specific tag

--- api/BackEndAutomation/features/BookApi.feature
+++ api/BackEndAutomation/features/BookApi.feature
@@ -1,12 +1,12 @@
 Feature: Verify if Books are added and deleted using Library API
-  @smoke
+  @books
   Scenario: Verify addBook API functionality
     Given Book details which needs to be added to library
     When We execute  addBook POST method
     Then Book is successfully added
     And Status code of response is 200
 
-  @regression
+  @books
   Scenario Outline: Verify addBook API functionality #used for parametrization
     Given Book details with <isbn> and <aisle> to be added to library
     When We execute  addBook POST method
--- api/BackEndAutomation/features/environment.py
+++ api/BackEndAutomation/features/environment.py
@@ -9,12 +9,13 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
-    delete_url = getConfig()['API']['endpoint'] + apiResources.deleteBook
-    delete_response = requests.post(delete_url, json={
-        "ID": context.bookId}, headers={"Content-Type": "application/json"})
-    print(delete_response.status_code)
-    print(delete_response.text)
-    d = delete_response.json()
-    assert delete_response.status_code == 200
-    print("Assert success - status code 200")
-    assert d["msg"] == "book is successfully deleted"
+    if "books" in scenario.tags:  # we check the tag of scenario
+        delete_url = getConfig()['API']['endpoint'] + apiResources.deleteBook
+        delete_response = requests.post(delete_url, json={
+            "ID": context.bookId}, headers={"Content-Type": "application/json"})
+        print(delete_response.status_code)
+        print(delete_response.text)
+        d = delete_response.json()
+        assert delete_response.status_code == 200
+        print("Assert success - status code 200")
+        assert d["msg"] == "book is successfully deleted"
