abbf7b80249c4db52f65dfe66d48096267d5c119
Alexander <sashapush@tut.by>
2023-02-20 13:10:43 +0100

    [PARAMIKO] ssh connection with simple commands execution

--- api/BackEndAutomation/sshConnection.py
+++ api/BackEndAutomation/sshConnection.py
@@ -3,6 +3,7 @@ import paramiko as par
 
 from api.BackEndAutomation.utils.configs import getConfig
 
+# Start connection
 host = getConfig()['SSH']['host']
 port = getConfig()['SSH']['port']
 user = getConfig()['SSH']['user']
@@ -11,4 +12,12 @@ ssh = par.SSHClient()  # creates a stream of connection
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)  # supress checking of ssh key
 ssh.connect(host, port, user, password)
 print("Connection was done")
+# Run commands
+# stdin, stdout, stderr = ssh.exec_command("ls -a") #Paramiko object
+stdin, stdout, stderr = ssh.exec_command("cat demo.txt")
+
+# print(stdout.readlines()) should be commented since it'll conflict with .readLines() below
+print(stderr.readlines())
+lines = stdout.readlines()
+print(lines[1])
 ssh.close()
